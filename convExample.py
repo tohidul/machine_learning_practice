ce693e5b4193710f7beb67c4de665659be602e2f
Tohidul Islam <tohidul.asif.96@gmail.com>
2022-08-26 15:50:29 +0600

    added a convolutional neural network exercise

--- convExample.py
+++ convExample.py
@@ -1,27 +1,54 @@
 import tensorflow as tf
 from tensorflow import keras
 
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('accuracy')>0.91):
+            print("\nAccuracy is greater than 91. closing the training")
+            self.model.stop_training = True
+
+callbacks = myCallback()
+
+# Load the Fashion MNIST dataset
+fmnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = fmnist.load_data()
+
+# Normalize the pixel values
+training_images = training_images / 255.0
+test_images = test_images / 255.0
+
+# Define the model
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28,28,1)),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
+
+    # Add convolutions and max pooling
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    # tf.keras.layers.MaxPooling2D(2, 2),
+
+    # Add the same layers as before
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
-fashionMNIST = tf.keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashionMNIST.load_data()
-train_images = train_images/255.0
-test_images = test_images/255.0
-
-model.compile(optimizer=tf.optimizers.Adam(),
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-model.fit(train_images, train_labels, epochs=30)
-model.evaluate(test_images, test_labels)
-
-
+# Print the model summary
 model.summary()
+
+# Use same settings
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+print(f'\nMODEL TRAINING:')
+model.fit(training_images, training_labels, epochs=5, callbacks = [callbacks])
+
+# Evaluate on the test set
+print(f'\nMODEL EVALUATION:')
+test_loss = model.evaluate(test_images, test_labels)
+
+
+
+
+# model.summary()
